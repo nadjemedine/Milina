50e0575b410349bf604bcd84f8cca5139bffdb4f
lolo06dev <lolo.dev.06@gmail.com>
2026-09-24 16:19:17 +0100

    chore: remove temporary scripts

--- fix_communes.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('src/lib/communes.json'));
-
-data["M'Sila"] = data["M'sila"] || data["M'Sila"];
-data["Bordj Bou Arreridj"] = data["Bordj Bou Arréridj"] || data["Bordj Bou Arreridj"];
-data["El M'Ghair"] = data["El M'ghair"] || data["El M'Ghair"];
-
-fs.writeFileSync('src/lib/communes.json', JSON.stringify(data));
-console.log('Fixed');
